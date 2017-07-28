defdab4a5da20546e74277267d16610ffa463d16
Saeed Tabrizi <saeed.tabrizi@gmail.com>
2017-07-28 19:12:12 +0430

    fix uuid utils and validation  tests .

---
+++ test/utils/UuidUtils.test.ts
@@ -0,0 +1,26 @@
+import "jest";
+import { uuid } from "../../src/utils/index";
+
+// jest.resetAllMocks();
+// jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+
+// tslint:disable:no-empty
+beforeAll(() => { });
+
+beforeEach(() => {
+
+});
+
+afterAll(() => { });
+
+afterEach(() => { });
+
+describe("UUID tests", async () => {
+
+  it("checks generate uuid version 4", async () => {
+    expect.assertions(1);
+    const actual = uuid();
+    expect(actual.length).toEqual(36) ;
+  });
+
+});
