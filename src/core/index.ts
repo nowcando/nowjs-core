9049469625aea69adf6d04a69453c6e00babce90
Saeed Tabrizi <saeed.tabrizi@gmail.com>
2017-07-31 21:09:53 +0430

    fix configuraton import

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,3 +3,8 @@ export * from "./IService";
 export * from "./IApplicationService";
 export * from "./IProvider";
 export * from "./IRepository";
+export * from "./Comparator";
+export * from "./Func";
+export * from "./IAsyncEnumerable";
+export * from "./IEnumerable";
+export * from "./Predicate";
