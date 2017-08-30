d27801801b417aad76cc88df1b23f6650f08dc22
Saeed Tabrizi <saeed.tabrizi@gmail.com>
2017-08-30 22:06:37 +0430

    feat: aad bignumber to Number prototype .

--- src/utils/NumberUtils.ts
+++ src/utils/NumberUtils.ts
@@ -1,4 +1,23 @@
+import { BigNumberOptions, BigNumberType } from "../computation/index";
+import {BigNumber} from "../computation/math/index";
 
 export function isNumberType(x: any): x is number {
     return typeof x === "number";
 }
+
+export function bigNumber(value?: BigNumberType, options?: BigNumberOptions): BigNumber {
+    return new BigNumber(value, options);
+}
+
+declare global {
+    // tslint:disable-next-line:interface-name
+    interface Number {
+        toBigNumber(options?: BigNumberOptions): BigNumber;
+    }
+}
+
+export function toBigNumber(options?: BigNumberOptions): BigNumber {
+    return new BigNumber(this, options);
+}
+
+Number.prototype.toBigNumber = toBigNumber;
