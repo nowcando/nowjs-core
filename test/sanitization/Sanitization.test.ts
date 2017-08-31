dc326644a1845ab64b83346cd2fe7b2642c3b122
Saeed Tabrizi <saeed.tabrizi@gmail.com>
2017-08-31 21:15:46 +0430

    fix: test description .

--- test/sanitization/Sanitization.test.ts
+++ test/sanitization/Sanitization.test.ts
@@ -19,69 +19,69 @@ afterAll(() => { });
 
 afterEach(() => { });
 
-describe("Lib::Sanitization:Sanitization.", async () => {
+describe("Sanitization", async () => {
+    describe("HtmlSanitizer", async () => {
+        it("checks html sanitization return passed string.", async () => {
+            expect.assertions(1);
+            const value = "<ul><li>hel<span>ddd</span>lo</li></ul>";
+            Sanitization.define("htmlSanitizer", new HtmlSanitizer());
 
-     it("checks html sanitization return passed string.", async () => {
-        expect.assertions(1);
-        const value = "<ul><li>hel<span>ddd</span>lo</li></ul>";
-        Sanitization.define("htmlSanitizer", new HtmlSanitizer());
+            const result = await Sanitization.sanitize(value, "htmlSanitizer");
+            expect(result).toEqual(value);
 
-        const result = await Sanitization.sanitize(value, "htmlSanitizer");
-        expect(result).toEqual(value);
+        });
 
+        it("checks html sanitization special tags return passed string.", async () => {
+            expect.assertions(1);
+            const value = "<ul><li>hel<span>lili</span>lo</li></ul>";
+            Sanitization.define("htmlSanitizer", new HtmlSanitizer({ AllowedTags: ["ul", "li", "span"] }));
+
+            const result = await Sanitization.sanitize(value, "htmlSanitizer");
+            expect(result).toEqual(value);
+
+        });
+
+        it("checks html sanitization special tags return empty string.", async () => {
+            expect.assertions(1);
+            const value = "<ul><li>hel<span>lili</span>lo</li></ul>";
+            Sanitization.define("htmlSanitizer", new HtmlSanitizer({ AllowedTags: ["post"] }));
+
+            const result = await Sanitization.sanitize(value, "htmlSanitizer");
+            expect(result).toEqual("");
+
+        });
+
+        it("checks html attributes sanitization return passed string.", async () => {
+            expect.assertions(1);
+            const value = '<ul id="menu1" name="name1"><li>hel<span>ddd</span>lo</li></ul>';
+            Sanitization.define("htmlSanitizer", new HtmlSanitizer());
+
+            const result = await Sanitization.sanitize(value, "htmlSanitizer");
+            expect(result).toEqual('<ul id="menu1" name="name1" ><li>hel<span>ddd</span>lo</li></ul>');
+
+        });
+
+        it("checks html sanitization remove", async () => {
+            expect.assertions(1);
+            const value = "<ul><li>hel<span>ddd</span>lo</li></ul>";
+            Sanitization.define("htmlSanitizer", new HtmlSanitizer());
+
+            const actual = Sanitization.remove("htmlSanitizer");
+            expect(actual).toEqual(true);
+
+        });
+
+        it("checks html sanitization contains", async () => {
+            expect.assertions(2);
+            const value = "<ul><li>hel<span>ddd</span>lo</li></ul>";
+            const snt = new HtmlSanitizer();
+            const actualName = snt.Name;
+            Sanitization.define("htmlSanitizer", snt);
+
+            const actual = Sanitization.contains("htmlSanitizer");
+            expect(actualName).toEqual("HtmlSanitizer");
+            expect(actual).toEqual(true);
+
+        });
     });
-
-     it("checks html sanitization special tags return passed string.", async () => {
-        expect.assertions(1);
-        const value = "<ul><li>hel<span>lili</span>lo</li></ul>";
-        Sanitization.define("htmlSanitizer", new HtmlSanitizer({AllowedTags: [ "ul", "li", "span"]}));
-
-        const result = await Sanitization.sanitize(value, "htmlSanitizer");
-        expect(result).toEqual(value);
-
-    });
-
-     it("checks html sanitization special tags return empty string.", async () => {
-        expect.assertions(1);
-        const value = "<ul><li>hel<span>lili</span>lo</li></ul>";
-        Sanitization.define("htmlSanitizer", new HtmlSanitizer({AllowedTags: ["post"]}));
-
-        const result = await Sanitization.sanitize(value, "htmlSanitizer");
-        expect(result).toEqual("");
-
-    });
-
-     it("checks html attributes sanitization return passed string.", async () => {
-        expect.assertions(1);
-        const value = '<ul id="menu1" name="name1"><li>hel<span>ddd</span>lo</li></ul>';
-        Sanitization.define("htmlSanitizer", new HtmlSanitizer());
-
-        const result = await Sanitization.sanitize(value, "htmlSanitizer");
-        expect(result).toEqual('<ul id="menu1" name="name1" ><li>hel<span>ddd</span>lo</li></ul>');
-
-    });
-
-     it("checks html sanitization remove", async () => {
-        expect.assertions(1);
-        const value = "<ul><li>hel<span>ddd</span>lo</li></ul>";
-        Sanitization.define("htmlSanitizer", new HtmlSanitizer());
-
-        const actual = Sanitization.remove("htmlSanitizer");
-        expect(actual).toEqual(true);
-
-    });
-
-     it("checks html sanitization contains", async () => {
-        expect.assertions(2);
-        const value = "<ul><li>hel<span>ddd</span>lo</li></ul>";
-        const snt = new HtmlSanitizer();
-        const actualName = snt.Name;
-        Sanitization.define("htmlSanitizer", snt);
-
-        const actual = Sanitization.contains("htmlSanitizer");
-        expect(actualName).toEqual("HtmlSanitizer");
-        expect(actual).toEqual(true);
-
-    });
-
 });
