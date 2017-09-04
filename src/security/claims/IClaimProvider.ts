d730f9c3863c82bde2a2ebc2ef8bb7441ee99897
Saeed Tabrizi <saeed.tabrizi@gmail.com>
2017-09-05 00:30:04 +0430

    fix: some changes in membership, role and claim providers .

--- src/security/claims/IClaimProvider.ts
+++ src/security/claims/IClaimProvider.ts
@@ -4,17 +4,27 @@ import { IDType, IQueryOptions, IQueryResult } from "../../data/index";
 import { Claim, Scope } from "./index";
 
 export interface IClaimProvider<TClaim extends Claim, TScope extends Scope> extends IProvider {
-        getUserClaims(tenantID: IDType, app: string , usersid: IDType,
-                      ...types: string[] ): Promise<TClaim[]>;
-        addUserClaim(tenantID: IDType, app: string , usersid: IDType,
+        getUserClaims(tenantID: IDType, app: string, usersid: IDType[], scopes: string[],
+                      ...types: string[]): Promise<TClaim[]>;
+        addUserClaim(tenantID: IDType, app: string, usersid: IDType, scope: string,
                      ...claims: TClaim[]): Promise<TClaim[]>;
-        updateUserClaim(tenantID: IDType, app: string , usersid: IDType ,
+        updateUserClaim(tenantID: IDType, app: string, usersid: IDType, scope: string,
                         ...claims: TClaim[]): Promise<TClaim[]>;
-        removeUserClaim(tenantID: IDType, app: string , usersid: IDType ,
-                        ...claims: string[]): Promise<boolean>;
-        addScope(tenantID: IDType, app: string , ...scopes: TScope[]): Promise<TScope[]>;
-        updateScope(tenantID: IDType, app: string , ...scopes: TScope[]): Promise<TScope[]>;
-        removeScope(tenantID: IDType, app: string , ...scopes: string[]): Promise<boolean[]>;
-        getScopes(tenantID: IDType, app: string , ...scopes: string[]): Promise<TScope[]>;
+        removeUserClaim(tenantID: IDType, app: string, usersid: IDType, scope: string,
+                        ...claims: TClaim[]): Promise<boolean>;
+
+        getRoleClaims(tenantID: IDType, app: string, role: string[], scopes: string[],
+                      ...types: string[]): Promise<TClaim[]>;
+        addRoleClaim(tenantID: IDType, app: string, role: string, scope: string,
+                     ...claims: TClaim[]): Promise<TClaim[]>;
+        updateRoleClaim(tenantID: IDType, app: string, role: string, scope: string,
+                        ...claims: TClaim[]): Promise<TClaim[]>;
+        removeRoleClaim(tenantID: IDType, app: string, role: string, scope: string,
+                        ...claims: TClaim[]): Promise<boolean>;
+
+        addScope(tenantID: IDType, app: string, scope: TScope): Promise<TScope>;
+        updateScope(tenantID: IDType, app: string, scope: TScope): Promise<TScope>;
+        removeScope(tenantID: IDType, app: string, ...scopes: string[]): Promise<boolean>;
+        getScopes(tenantID: IDType, app: string, ...scopes: string[]): Promise<TScope[]>;
 
 }
--- src/security/membership/IMembershipProvider.ts
+++ src/security/membership/IMembershipProvider.ts
@@ -3,47 +3,51 @@ import { IProvider } from "../../core/IProvider";
 import { IDType, IQueryOptions, IQueryResult } from "../../data";
 
 export interface IMembershipProvider<TUser, TProfile, TMeta> extends IProvider {
-          create(tenantID: IDType, app: string, user: TUser, meta?: TMeta): Promise<TUser>;
-          delete(tenantID: IDType, app: string, userID: IDType, meta?: TMeta): Promise<TUser>;
-          activate(tenantID: IDType, app: string, userID: IDType, notes?: string, meta?: TMeta): Promise<TUser>;
-          suspend(tenantID: IDType, app: string, userID: IDType, notes?: string, meta?: TMeta): Promise<TUser>;
-          isExistsByUserID(tenantID: IDType, app: string, userID: IDType): Promise<boolean>;
-          getUserID(tenantID: IDType, app: string, userID: IDType): Promise<TUser>;
+          createUser(tenantID: IDType, app: string, user: TUser, meta?: TMeta): Promise<TUser>;
+          deleteUser(tenantID: IDType, app: string, userID: IDType, meta?: TMeta): Promise<TUser>;
+          activateUser(tenantID: IDType, app: string, userID: IDType, notes?: string, meta?: TMeta): Promise<TUser>;
+          suspendUser(tenantID: IDType, app: string, userID: IDType,
+                      causeID: number , notes?: string, meta?: TMeta): Promise<TUser>;
+          isUserExistsByUserID(tenantID: IDType, app: string, userID: IDType): Promise<boolean>;
+          getUserByID(tenantID: IDType, app: string, userID: IDType): Promise<TUser>;
           getUsersByID(tenantID: IDType, app: string, ...userID: IDType[]): Promise<TUser[]>;
-          updateProfile(tenantID: IDType, app: string, userID: IDType, profile: TProfile, meta?: TMeta): Promise<TUser>;
+          updateUserProfile(tenantID: IDType, app: string, userID: IDType,
+                            profile: TProfile, meta?: TMeta): Promise<TUser>;
           getUsers(tenantID: IDType, app: string, options?: IQueryOptions): Promise<IQueryResult<TUser>>;
 }
 
 export interface IMobileMembershipProvider<TUser, TProfile, TMeta> extends IMembershipProvider<TUser, TProfile, TMeta> {
-        isExistsByMobile(tenantID: IDType, app: string, mobile: string): Promise<boolean>;
+        isUserExistsByMobile(tenantID: IDType, app: string, mobile: string): Promise<boolean>;
         getUserByMobile(tenantID: IDType, app: string, mobile: string): Promise<TUser>;
-        validateMobile(tenantID: IDType, app: string, mobile: string ,
-                       tryCount: boolean , meta?: TMeta): Promise<TUser>;
-        validateMobileTwoFactorCode(tenantID: IDType, app: string, tempToken: string  ,
-                                    tfaCode: string , tryCount: boolean , meta?: TMeta): Promise<TUser>;
+        getUsersByMobiles(tenantID: IDType, app: string, ...mobile: string[]): Promise<TUser[]>;
+        validateUserByMobile(tenantID: IDType, app: string, mobile: string ,
+                             tryCount: boolean , meta?: TMeta): Promise<TUser>;
+        validateUserMobileTwoFactorCode(tenantID: IDType, app: string, tempToken: string  ,
+                                        tfaCode: string , tryCount: boolean , meta?: TMeta): Promise<TUser>;
         generateMobileTwoFactorCode(tenantID: IDType, app: string): string;
         sendMobileTwoFactorCode(tenantID: IDType, app: string,
-                                mobile: string , tfaCode: string , meta?: TMeta): Promise<TUser>;
+                                mobile: string , tfaCode: string , expiresAt?: Date , meta?: TMeta): Promise<TUser>;
         enableMobileTwoFactor(tenantID: IDType, app: string, userID: IDType): Promise<TUser>;
         disableMobileTwoFactor(tenantID: IDType, app: string, userID: IDType): Promise<TUser>;
-        updateDefaultMobile(tenantID: IDType, app: string,
-                            userID: IDType , mobile: string , meta?: TMeta): Promise<TUser>;
-        addMobile(tenantID: IDType, app: string, userID: IDType , mobile: string , meta?: TMeta): Promise<TUser>;
-        removeMobile(tenantID: IDType, app: string, userID: IDType , mobile: string , meta?: TMeta): Promise<TUser>;
+        updateUserDefaultMobile(tenantID: IDType, app: string,
+                                userID: IDType , mobile: string , meta?: TMeta): Promise<TUser>;
+        addUserMobile(tenantID: IDType, app: string, userid: IDType , mobile: string , meta?: TMeta): Promise<TUser>;
+        removeUserMobile(tenantID: IDType, app: string, userid: IDType , mobile: string , meta?: TMeta): Promise<TUser>;
  }
 
 export interface IUsernameMembershipProvider<TUser, TProfile, TMeta> extends
         IMembershipProvider<TUser, TProfile, TMeta> {
-        isExistsByUsername(tenantID: IDType, app: string, username: string): Promise<boolean>;
+        isUserExistsByUsername(tenantID: IDType, app: string, username: string): Promise<boolean>;
         getUserByUsername(tenantID: IDType, app: string, username: string): Promise<TUser>;
-        validateUsername(tenantID: IDType, app: string, username: string , password: string ,
-                         tryCount: boolean , meta?: TMeta): Promise<TUser>;
+        getUsersByUsername(tenantID: IDType, app: string, ...username: string[]): Promise<TUser[]>;
+        validateUserByUsername(tenantID: IDType, app: string, username: string , password: string ,
+                               tryCount: boolean , meta?: TMeta): Promise<TUser>;
         generatePassword(tenantID: IDType, app: string): string;
         generateSalt(tenantID: IDType, app: string): string;
         generateConfirmCode(tenantID: IDType, app: string): string;
         sendUserConfirmCode(tenantID: IDType, app: string,
-                            username: string , confirmCode: string , meta?: TMeta): Promise<TUser>;
-        validateUserConfirmCode(tenantID: IDType, app: string , confirmCode: string ,
+                            username: string , confirmCode: string, expiresAt?: Date , meta?: TMeta): Promise<TUser>;
+        validateUserConfirmCode(tenantID: IDType, app: string , confirmCode: string  ,
                                 tryCount: boolean , meta?: TMeta): Promise<TUser>;
         changePassowrd(tenantID: IDType, app: string, username: string , oldPassword: string , newPassword: string ,
                        tryCount: boolean , meta?: TMeta): Promise<TUser>;
@@ -58,17 +62,17 @@ export interface IUsernameMembershipProvider<TUser, TProfile, TMeta> extends
 
 export interface IEmailMembershipProvider<TUser, TProfile, TMeta> extends
         IMembershipProvider<TUser, TProfile, TMeta> {
-        isExistsByEmail(tenantID: IDType, app: string, username: string): Promise<boolean>;
-        getUserByEmail(tenantID: IDType, app: string, username: string): Promise<TUser>;
-
+        isUserExistsByEmail(tenantID: IDType, app: string, email: string): Promise<boolean>;
+        getUserByEmail(tenantID: IDType, app: string, email: string): Promise<TUser>;
+        getUsersByEmail(tenantID: IDType, app: string, ...email: string[]): Promise<TUser[]>;
         generateConfirmCode(tenantID: IDType, app: string): string;
         sendEmailConfirmCode(tenantID: IDType, app: string,
-                             email: string , confirmCode: string , meta?: TMeta): Promise<TUser>;
-        validateEmailConfirmCode(tenantID: IDType, app: string , confirmCode: string ,
-                                 tryCount: boolean , meta?: TMeta): Promise<TUser>;
+                             email: string , confirmCode: string , expiresAt?: Date , meta?: TMeta): Promise<TUser>;
+        validateUserEmailConfirmCode(tenantID: IDType, app: string , confirmCode: string ,
+                                     tryCount: boolean , meta?: TMeta): Promise<TUser>;
 
-        updateDefaultEmail(tenantID: IDType, app: string,
-                           username: string , email: string , meta?: TMeta): Promise<TUser>;
-        addEmail(tenantID: IDType, app: string, username: string , email: string , meta?: TMeta): Promise<TUser>;
-        removeEmail(tenantID: IDType, app: string, username: string , email: string , meta?: TMeta): Promise<TUser>;
+        updateUserDefaultEmail(tenantID: IDType, app: string,
+                               username: string , email: string , meta?: TMeta): Promise<TUser>;
+        addUserEmail(tenantID: IDType, app: string, userid: IDType , email: string , meta?: TMeta): Promise<TUser>;
+        removeUserEmail(tenantID: IDType, app: string, userid: IDType, email: string , meta?: TMeta): Promise<TUser>;
 }
