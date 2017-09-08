33781c44278f6ef48a8425add02a689045342d06
Saeed Tabrizi <saeed.tabrizi@gmail.com>
2017-09-08 19:21:19 +0430

    fix: deepAssign for date and non keys objects .

--- test/utils/ObjectUtils.test.ts
+++ test/utils/ObjectUtils.test.ts
@@ -30,7 +30,7 @@ describe("ObjectUtils", async () => {
     });
     it("checks deepAssign", async () => {
         expect.assertions(4);
-        const tobj = { a: 1 , h: { k: 5} , c: 9};
+        const tobj = { a: 1, h: { k: 5 }, c: 9 };
         const tobj1 = { h: { b: { c: 1 } } };
         const tobj2 = { h: { b: { c: 4 }, d: { e: 1 } } };
         const actual = deepAssign(tobj, tobj1, tobj2);
@@ -39,9 +39,25 @@ describe("ObjectUtils", async () => {
         expect(actual.h.b.c).toEqual(4);
         expect(actual.h.d.e).toEqual(1);
     });
+
+    it("checks deepAssign to object that no have any keys", async () => {
+        expect.assertions(5);
+        const dt1 = new Date(1970, 1, 1, 0, 0, 0, 0);
+        const dt2 = new Date(1980, 2, 2, 0, 0, 0, 0);
+        const tobj = { a: dt1, h: { k: 5 }, c: 9 };
+        const tobj1 = { h: { b: { c: 1 } }, m: dt2  };
+        const tobj2 = { h: { b: { c: 4 }, d: { e: 1 } } };
+        const actual = deepAssign({}, tobj, tobj1, tobj2);
+        expect(actual.a).toEqual(dt1);
+        expect(actual.c).toEqual(9);
+        expect(actual.m).toEqual(dt2);
+        expect(actual.h.b.c).toEqual(4);
+        expect(actual.h.d.e).toEqual(1);
+    });
+
     it("checks cloneObject", async () => {
         expect.assertions(3);
-        const tobj = { a: 1 , h: { k: 5} , c: 9};
+        const tobj = { a: 1, h: { k: 5 }, c: 9 };
         const actual = cloneObject(tobj);
         expect(actual.a).toEqual(1);
         expect(actual.c).toEqual(9);
