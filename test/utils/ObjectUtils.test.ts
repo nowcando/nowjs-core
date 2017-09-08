4295379d54cf514dd05f002b65dd77afdde1ef47
Saeed Tabrizi <saeed.tabrizi@gmail.com>
2017-09-08 19:37:27 +0430

    test: add some test to deepAssign .

--- test/utils/ObjectUtils.test.ts
+++ test/utils/ObjectUtils.test.ts
@@ -45,12 +45,33 @@ describe("ObjectUtils", async () => {
         const dt1 = new Date(1970, 1, 1, 0, 0, 0, 0);
         const dt2 = new Date(1980, 2, 2, 0, 0, 0, 0);
         const tobj = { a: dt1, h: { k: 5 }, c: 9 };
-        const tobj1 = { h: { b: { c: 1 } }, m: dt2  };
+        const tobj1 = { h: { b: { c: 1 } }, m: dt2 };
         const tobj2 = { h: { b: { c: 4 }, d: { e: 1 } } };
         const actual = deepAssign({}, tobj, tobj1, tobj2);
         expect(actual.a).toEqual(dt1);
         expect(actual.c).toEqual(9);
         expect(actual.m).toEqual(dt2);
+        expect(actual.h.b.c).toEqual(4);
+        expect(actual.h.d.e).toEqual(1);
+    });
+
+    it("checks deepAssign to object mixed date and array and function", async () => {
+        expect.assertions(7);
+        const dt1 = new Date(1970, 1, 1, 0, 0, 0, 0);
+        const dt2 = new Date(1980, 2, 2, 0, 0, 0, 0);
+        const arr1 = [10, 11, 12, 13, 14];
+        const arr2 = [20, 21, 22, 23, 24];
+        const fn1 = () => 35;
+        const fn2 = () => 45;
+        const tobj = { a: dt1, h: { k: 5 }, c: 9 , age: fn1 };
+        const tobj1 = { h: { b: { c: 1 } }, m: dt2 , n : arr1 };
+        const tobj2 = { h: { b: { c: 4 }, d: { e: 1 } } , n: arr2 , age: fn2 };
+        const actual = deepAssign({}, tobj, tobj1, tobj2);
+        expect(actual.a).toEqual(dt1);
+        expect(actual.c).toEqual(9);
+        expect(actual.m).toEqual(dt2);
+        expect(actual.n).toEqual(arr2);
+        expect(actual.age).toEqual(fn2);
         expect(actual.h.b.c).toEqual(4);
         expect(actual.h.d.e).toEqual(1);
     });
