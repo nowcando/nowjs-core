819f6d87d1889b0ae2d946ad3734e0ba0264cad5
Saeed Tabrizi <saeed.tabrizi@gmail.com>
2017-09-19 02:52:02 +0430

    fix: add ITenantConfigurationProvider , some   change in IPhoneMembershipProvider methods.

--- src/configuration/IConfigurationProvider.ts
+++ src/configuration/IConfigurationProvider.ts
@@ -1,5 +1,6 @@
 
 import { IProvider } from "../core/IProvider";
+import { IDType } from "../data/index";
 
 export class ConfigSection<T> {
 
@@ -25,11 +26,21 @@ export class ConfigSection<T> {
 
 }
 
+// tslint:disable-next-line:no-empty-interface
 export interface IConfigurationProvider extends IProvider {
+
+}
+export interface ILocalConfigurationProvider extends IConfigurationProvider {
     load(): Promise<void>;
     save(): Promise<void>;
     get<T>(path: string, defaultValue?: T): T;
     getSection<T>(path: string, defaultValue?: T): ConfigSection<T>;
     set<T>(path: string, value: T): boolean;
     setSection<T>(section: ConfigSection<T>): boolean;
+}
+export interface ITenantConfigurationProvider extends IConfigurationProvider {
+    get<T>(tenantID: IDType, app: string, path: string, defaultValue?: T): Promise<T>;
+    getSection<T>(tenantID: IDType, app: string, path: string, defaultValue?: T): Promise<ConfigSection<T>>;
+    set<T>(tenantID: IDType, app: string, path: string, value: T): Promise<boolean>;
+    setSection<T>(tenantID: IDType, app: string, section: ConfigSection<T>): Promise<boolean>;
 }
