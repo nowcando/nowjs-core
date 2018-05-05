3cff1a84f4af9c5d27be27f4f79aaa0479a47625
Saeed Tabrizi <saeed.tabrizi@gmail.com>
2018-05-05 18:28:13 +0430

    test : add color utils test and dummy test  for run in node 10.

--- src/utils/ColorUtils.ts
+++ src/utils/ColorUtils.ts
@@ -1,4 +1,6 @@
-
+// taken from https://github.com/bgrins/TinyColor/blob/master/tinycolor.js
+// https://github.com/bgrins/TinyColor
+// Brian Grinstead, MIT License
 function pad2(c: string) {
     return c.length === 1 ? "0" + c : "" + c;
 }
@@ -43,13 +45,19 @@ export class ColorUtils {
      */
     public static getBrightness(r: number | string, g?: number, b?: number): number {
 
-        const rgb = ColorUtils.toRGB(r, g, b);
-        return ((r as number) * 299 + g * 587 + b * 114) / 1000;
+        const rgb = ColorUtils.toRgb(r, g, b);
+        return (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000;
     }
 
-    public static toRGB(r: number | string | object, g?: number, b?: number, a?: number) {
+    public static toRgb(r: number | string | object, g?: number, b?: number, a?: number) {
         if (!g && !b && typeof r === "string") {
-            const c = r.toString().substring(1);      // strip #
+            let c = r.toString().substring(1);      // strip #
+            if (c.length === 3) {
+                const tr = c[0] + c[0] || "0";
+                const tg = c[1] + c[1] || "0";
+                const tb = c[2] + c[2] || "0";
+                c = `${tr + tg + tb}`;
+            }
             const rgb = parseInt(c, 16);   // convert rrggbb to decimal
             // tslint:disable:no-bitwise
             r = (rgb >> 16) & 0xff;  // extract red
@@ -57,14 +65,14 @@ export class ColorUtils {
             b = (rgb >> 0) & 0xff;  // extract blue
         }
         if (typeof r === "object") {
-            const c: any =  r;
+            const c: any = r;
             r = c.r;
             g = c.g;
             b = c.b;
             a = c.a;
         }
         const mr = r as number;
-        return {r: mr, g, b , a};
+        return { r: mr, g, b, a };
     }
     public static rgbToHex(r: number, g: number, b: number, allow3Char?: boolean) {
         const hex = [
@@ -74,7 +82,7 @@ export class ColorUtils {
         ];
         // Return a 3 character hex if possible
         if (allow3Char && hex[0].charAt(0) === hex[0].charAt(1) &&
-             hex[1].charAt(0) === hex[1].charAt(1) && hex[2].charAt(0) === hex[2].charAt(1)) {
+            hex[1].charAt(0) === hex[1].charAt(1) && hex[2].charAt(0) === hex[2].charAt(1)) {
             return hex[0].charAt(0) + hex[1].charAt(0) + hex[2].charAt(0);
         }
         return hex.join("");
@@ -86,10 +94,22 @@ export class ColorUtils {
         return "#" + ColorUtils.toHex(r, g, b, allow3Char);
     }
 
-    public static toHSL(r: number | string, g?: number, b?: number , a?: number) {
-        const rgb = ColorUtils.toRGB(r, g, b);
+    public static toHsl(r: number | string, g?: number, b?: number, a?: number) {
+        const rgb = ColorUtils.toRgb(r, g, b);
         const hsl = ColorUtils.rgbToHsl(rgb.r, rgb.g, rgb.b);
         return { h: hsl.h * 360, s: hsl.s, l: hsl.l, a };
+    }
+
+    public static toHslString(r: number | string, g?: number, b?: number, a?: number) {
+        const rgb = ColorUtils.toRgb(r, g, b);
+        const hsl = ColorUtils.rgbToHsl(rgb.r, rgb.g, rgb.b);
+        // tslint:disable-next-line:one-variable-per-declaration
+        const h = Math.round(hsl.h * 360),
+            s = Math.round(hsl.s * 100),
+            l = Math.round(hsl.l * 100);
+        return (rgb.a === 1) ?
+            "hsl(" + h + ", " + s + "%, " + l + "%)" :
+            "hsla(" + h + ", " + s + "%, " + l + "%, " + rgb.a + ")";
     }
 
     public static rgbToHsl(r: number, g?: number, b?: number) {
@@ -105,7 +125,7 @@ export class ColorUtils {
 
         if (max === min) {
             h = s = 0; // achromatic
-        }   else {
+        } else {
             const d = max - min;
             s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
             switch (max) {
@@ -120,18 +140,47 @@ export class ColorUtils {
         return { h, s, l };
     }
 
+    public static hue2rgb(p: number, q: number, t: number) {
+        if (t < 0) { t += 1; }
+        if (t > 1) { t -= 1; }
+        if (t < 1 / 6) { return p + (q - p) * 6 * t; }
+        if (t < 1 / 2) { return q; }
+        if (t < 2 / 3) { return p + (q - p) * (2 / 3 - t) * 6; }
+        return p;
+    }
+
+    public static hslToRgb(h: number, s: number, l: number) {
+        let r, g, b;
+
+        h = bound01(h, 360);
+        s = bound01(s, 100);
+        l = bound01(l, 100);
+
+        if (s === 0) {
+            r = g = b = l; // achromatic
+        } else {
+            const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            const p = 2 * l - q;
+            r = ColorUtils.hue2rgb(p, q, h + 1 / 3);
+            g = ColorUtils.hue2rgb(p, q, h);
+            b = ColorUtils.hue2rgb(p, q, h - 1 / 3);
+        }
+
+        return { r: r * 255, g: g * 255, b: b * 255 };
+    }
+
     public static getLuminance(r: number | string, g?: number, b?: number) {
         // http://www.w3.org/TR/2008/REC-WCAG20-20081211/#relativeluminancedef
-        const rgb = ColorUtils.toRGB(r, g, b);
+        const rgb = ColorUtils.toRgb(r, g, b);
         // tslint:disable-next-line:one-variable-per-declaration
         let RsRGB, GsRGB, BsRGB, R, G, B;
         RsRGB = rgb.r / 255;
         GsRGB = rgb.g / 255;
         BsRGB = rgb.b / 255;
 
-        if (RsRGB <= 0.03928) {R = RsRGB / 12.92; } else {R = Math.pow(((RsRGB + 0.055) / 1.055), 2.4); }
-        if (GsRGB <= 0.03928) {G = GsRGB / 12.92; } else {G = Math.pow(((GsRGB + 0.055) / 1.055), 2.4); }
-        if (BsRGB <= 0.03928) {B = BsRGB / 12.92; } else {B = Math.pow(((BsRGB + 0.055) / 1.055), 2.4); }
+        if (RsRGB <= 0.03928) { R = RsRGB / 12.92; } else { R = Math.pow(((RsRGB + 0.055) / 1.055), 2.4); }
+        if (GsRGB <= 0.03928) { G = GsRGB / 12.92; } else { G = Math.pow(((GsRGB + 0.055) / 1.055), 2.4); }
+        if (BsRGB <= 0.03928) { B = BsRGB / 12.92; } else { B = Math.pow(((BsRGB + 0.055) / 1.055), 2.4); }
         return (0.2126 * R) + (0.7152 * G) + (0.0722 * B);
     }
 
@@ -144,7 +193,7 @@ export class ColorUtils {
 
     public static desaturate(r: number | string, g?: number, b?: number, amount?: number) {
         amount = (amount === 0) ? 0 : (amount || 10);
-        const hsl = ColorUtils.toHSL(r, g, b);
+        const hsl = ColorUtils.toHsl(r, g, b);
         hsl.s -= amount / 100;
         hsl.s = clamp01(hsl.s);
         return hsl;
@@ -152,7 +201,7 @@ export class ColorUtils {
 
     public static saturate(r: number | string, g?: number, b?: number, amount?: number) {
         amount = (amount === 0) ? 0 : (amount || 10);
-        const hsl = ColorUtils.toHSL(r, g, b);
+        const hsl = ColorUtils.toHsl(r, g, b);
         hsl.s += amount / 100;
         hsl.s = clamp01(hsl.s);
         return hsl;
@@ -160,7 +209,7 @@ export class ColorUtils {
 
     public static lighten(r: number | string, g?: number, b?: number, amount?: number) {
         amount = (amount === 0) ? 0 : (amount || 10);
-        const hsl = ColorUtils.toHSL(r, g, b);
+        const hsl = ColorUtils.toHsl(r, g, b);
         hsl.l += amount / 100;
         hsl.l = clamp01(hsl.l);
         return hsl;
@@ -168,7 +217,7 @@ export class ColorUtils {
 
     public static brighten(r: number | string, g?: number, b?: number, amount?: number) {
         amount = (amount === 0) ? 0 : (amount || 10);
-        const rgb = ColorUtils.toRGB(r, g, b);
+        const rgb = ColorUtils.toRgb(r, g, b);
         rgb.r = Math.max(0, Math.min(255, rgb.r - Math.round(255 * - (amount / 100))));
         rgb.g = Math.max(0, Math.min(255, rgb.g - Math.round(255 * - (amount / 100))));
         rgb.b = Math.max(0, Math.min(255, rgb.b - Math.round(255 * - (amount / 100))));
@@ -177,17 +226,53 @@ export class ColorUtils {
 
     public static darken(r: number | string, g?: number, b?: number, amount?: number) {
         amount = (amount === 0) ? 0 : (amount || 10);
-        const hsl = ColorUtils.toHSL(r, g, b);
+        const hsl = ColorUtils.toHsl(r, g, b);
         hsl.l -= amount / 100;
         hsl.l = clamp01(hsl.l);
         return hsl;
     }
 
-    public static overlayColor(color: string , lightColor?: string, darkColor?: string) {
-        // if only first half of color is defined, repeat it
-        if (color.length < 5) {
-            color += color.slice(1);
-        }
-        return parseFloat((color.replace("#", "0x"))) > (0xffffff / 2) ? darkColor || "#333" : lightColor || "#fff";
+    public static overlayColorToRgbString(color: string, amount?: number, lightColor?: string, darkColor?: string) {
+        const rgb = ColorUtils.overlayColorToRgb(color, amount, lightColor, darkColor);
+        return ColorUtils.toHexString(rgb.r, rgb.g, rgb.b);
+    }
+    public static overlayColorToHsl(color: string, amount?: number, lightColor?: string, darkColor?: string) {
+        amount = amount || 80;
+        const hsl = ColorUtils.isDark(color) ?
+            ColorUtils.lighten(color, null, null, amount) :
+            ColorUtils.darken(color, null, null, amount);
+        return hsl;
+    }
+    public static overlayColorToRgb(color: string, amount?: number, lightColor?: string, darkColor?: string) {
+        const hsl = ColorUtils.overlayColorToHsl(color, amount, lightColor, darkColor);
+        const rgb = ColorUtils.hslToRgb(hsl.h * 360, hsl.s * 100, hsl.l * 100);
+        return rgb;
+    }
+
+    public static complement(color: string) {
+        const hsl = ColorUtils.toHsl(color);
+        hsl.h = (hsl.h + 180) % 360;
+        return hsl;
+    }
+
+    public static triad(color: string) {
+        const hsl = ColorUtils.toHsl(color);
+        const h = hsl.h;
+        return [
+            hsl,
+            { h: (h + 120) % 360, s: hsl.s, l: hsl.l },
+            { h: (h + 240) % 360, s: hsl.s, l: hsl.l },
+        ];
+    }
+
+    public static tetrad(color: string) {
+        const hsl = ColorUtils.toHsl(color);
+        const h = hsl.h;
+        return [
+            hsl,
+            { h: (h + 90) % 360, s: hsl.s, l: hsl.l },
+            { h: (h + 180) % 360, s: hsl.s, l: hsl.l },
+            { h: (h + 270) % 360, s: hsl.s, l: hsl.l },
+        ];
     }
 }
