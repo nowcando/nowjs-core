551506e8dd77aeb416373bb5abbe688229a929af
Saeed Tabrizi <saeed@nowcando.com>
2019-05-24 06:04:11 +0430

    fix: remove ILogger dependency

--- src/data/db/IDbRepository.ts
+++ src/data/db/IDbRepository.ts
@@ -1,11 +1,10 @@
 import { IRepository } from "../../core/index";
-import { ILoggingProvider } from "../../logging/index";
+
 
 export interface IDbRepository<TSource, TDb, TMigrationResult> extends IRepository {
     HasMigrationFeature: boolean;
     getSource(dbName?: string): Promise<TSource>;
     getDb(dbName?: string): Promise<TDb>;
-    getLogger(): ILoggingProvider;
     upgrade(dbName?: string, limit?: number): Promise<TMigrationResult>;
     downgrade(dbName?: string, limit?: number): Promise<TMigrationResult>;
 
